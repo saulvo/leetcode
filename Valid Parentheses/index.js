6e6a6243bac4f85f66244f102403b6976d76d155
vtson23597 <67084195+vtson23597@users.noreply.github.com>
2020-09-12 13:53:15 +0700

    update

---
+++ Valid Parentheses/index.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+   const brackets = {
+      '{': '}',
+      '(': ')',
+      '[': ']'
+   }
+   let heap = [];
+
+   for (let char of s) {
+      if (brackets[char]) {
+         heap.push(brackets[char]);
+      } else {
+         if (heap.pop() !== char) return false;
+      }
+   }
+
+   return (!heap.length);
+};
+
+const s = "()";
+console.log(isValid(s));
