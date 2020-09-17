8dee550d863116ef1dc71921a2f566fbafd7561c
vtson23597 <67084195+vtson23597@users.noreply.github.com>
2020-09-17 18:37:49 +0700

    17/09/2020

---
+++ MergeTwoSortedLists/index.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+/**
+ * Input: 1->2->4, 1->3->4
+ * Output: 1->1->2->3->4->4
+ **/
+
+var mergeTwoLists = function (l1, l2) {
+   let dummy = new ListNode(-1);
+   let head = dummy;
+
+   while (l1 !== null && l2 !== null) {
+      if (l1.val <= l2.val) {
+         dummy.next = l1;
+         l1 = l1.next;
+      } else {
+         dummy.next = l2;
+         l2 = l2.next;
+      }
+      dummy = dummy.next;
+   }
+   if (l1 !== null) {
+      dummy.next = l1;
+   } else {
+      dummy.next = l2;
+   }
+   return head.next;
+};
